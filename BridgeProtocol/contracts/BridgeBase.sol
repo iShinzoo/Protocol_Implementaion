b3064c7c2f0a015fdeab5c7a885a7569d573be1c
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2025-05-29 11:25:34 +0530

    feat: add bridge order protocol

---
+++ BridgeProtocol/contracts/BridgeBase.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract BridgeBase is ReentrancyGuard, Pausable, Ownable {
+    mapping(bytes32 => bool) public processedMessages;
+
+    modifier onlyValidAddress(address to) {
+        require(to != address(0), "Invalid address");
+        _;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+}
